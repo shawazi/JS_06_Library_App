06ecda442492b7a342a3148f79d4753a2e000fb1
shawazi <shawazisonfire@gmail.com>
2023-02-11 09:09:18 -0500

    lazy display

--- src/dist/main.js
+++ src/dist/main.js
@@ -1,8 +1,15 @@
 "use strict";
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const isbnInput = document.getElementById("isbn");
 const submitButton = document.getElementById("submit-btn");
 submitButton === null || submitButton === void 0 ? void 0 : submitButton.addEventListener("click", () => {
     console.log("test");
+    const titleInput = document.getElementById("title").value;
+    const authorInput = document.getElementById("author").value;
+    const isbnInput = document.getElementById("isbn").value;
+    const outputElement = document.createElement("div");
+    outputElement.setAttribute("class", "cards");
+    const info = document.createElement("p");
+    info.textContent = titleInput + " | " + authorInput + " | " + isbnInput + " | " + "X";
+    outputElement.appendChild(info);
+    const display = document.getElementById("display-container");
+    display === null || display === void 0 ? void 0 : display.appendChild(outputElement);
 });
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,26 @@
-const titleInput = document.getElementById("title");
-
-const authorInput = document.getElementById("author");
-
-const isbnInput = document.getElementById("isbn");
-
 const submitButton = document.getElementById("submit-btn");
 
 submitButton?.addEventListener("click", () => {
     console.log("test");
+
+    const titleInput = (<HTMLInputElement>document.getElementById("title")).value;
+
+    const authorInput = (<HTMLInputElement>document.getElementById("author")).value;
+
+    const isbnInput = (<HTMLInputElement>document.getElementById("isbn")).value;
+
+    const outputElement = document.createElement("div");
+
+    outputElement.setAttribute("class", "cards");
+
+    const info = document.createElement("p");
+
+    info.textContent = titleInput + " | " + authorInput + " | " + isbnInput + " | " + "X";
+
+    outputElement.appendChild(info);
+    
+    const display = document.getElementById("display-container");
+
+    display?.appendChild(outputElement);
+
 });
