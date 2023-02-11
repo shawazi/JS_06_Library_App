cf2bb7c7effa81fc4997628d7593f5974e3dcd59
shawazi <shawazisonfire@gmail.com>
2023-02-11 09:06:06 -0500

    figuring out display

--- src/dist/main.js
+++ src/dist/main.js
@@ -1,11 +1,8 @@
+"use strict";
 const titleInput = document.getElementById("title");
-
 const authorInput = document.getElementById("author");
-
 const isbnInput = document.getElementById("isbn");
-
 const submitButton = document.getElementById("submit-btn");
-
-submitButton?.addEventListener("click", () => {
+submitButton === null || submitButton === void 0 ? void 0 : submitButton.addEventListener("click", () => {
     console.log("test");
 });
